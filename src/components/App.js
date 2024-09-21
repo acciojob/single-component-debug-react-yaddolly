4d8fff5afe7760eec1d7fb692fbe7eb03092f676
AAKASH _YADAV <yaddolly>
2024-09-21 22:39:04 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,13 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <p id='main'>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        <h1>Heheh</h1>
+    </div>
+  )
+}
+
+export default App
